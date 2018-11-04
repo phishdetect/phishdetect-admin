f73244181589278259d4e75d021c00020177193b
Nex <nex@nex.sx>
2018-11-04 16:59:05 +0100

    Removed unused import

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import os
 from setuptools import setup
 
 description = "Web application to administer a PhishDetect Node"
